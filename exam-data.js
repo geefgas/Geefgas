6c970fc1195087a47b086bdc0bea9e39147cf173
geefgas <k.aissaoui@live.nl>
2025-12-14 00:32:59 +0100

    Update exam-data.js

--- exam-data.js
+++ exam-data.js
@@ -1,1 +1,47 @@
+// GeefGas - Test Exam Data (Taxi)
+
+const EXAM_QUESTIONS = [
+  {
+    id: 1,
+    question: "Wie heeft voorrang op dit kruispunt?",
+    answers: ["De taxi", "De fietser van rechts", "De tegenligger"],
+    correct: 1
+  },
+  {
+    id: 2,
+    question: "Wat moet je doen bij dit verkeersbord?",
+    answers: ["Stoppen", "Snelheid verminderen", "Voorrang verlenen"],
+    correct: 2
+  },
+  {
+    id: 3,
+    question: "Wat is correct rijgedrag bij slecht zicht?",
+    answers: [
+      "Zelfde snelheid aanhouden",
+      "Snelheid aanpassen en afstand vergroten",
+      "Dichter op je voorligger rijden"
+    ],
+    correct: 1
+  },
+  {
+    id: 4,
+    question: "Wanneer mag een taxi stoppen op een halte?",
+    answers: [
+      "Altijd",
+      "Alleen om passagiers te laten in- of uitstappen",
+      "Nooit"
+    ],
+    correct: 1
+  },
+  {
+    id: 5,
+    question: "Wat betekent een doorgetrokken streep?",
+    answers: [
+      "Inhalen toegestaan",
+      "Inhalen verboden",
+      "Alleen voor vrachtwagens"
+    ],
+    correct: 1
+  }
+];
 
